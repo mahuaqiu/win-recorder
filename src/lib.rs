d2f6c7a91379a53974965d315424487492d3c1d2
majy2026 <372840680@qq.com>
2026-05-31 01:56:30 +0800

    init: win-recorder Rust 项目骨架

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use pyo3::prelude::*;
+
+/// win-recorder: Windows 硬编录屏库
+#[pymodule]
+fn win_recorder(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    // TODO: 添加 WinRecorder 类
+    Ok(())
+}
